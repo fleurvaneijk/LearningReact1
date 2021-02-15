af8f05b22ff175ba37622bc06cead52b715851cf
Fleur <fleur.vaneijk99@gmail.com>
2021-02-15 17:08:58 +0100

    components

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,10 +1,21 @@
+/*
+    CLASS COMPONENT:
+
+    - Class components are more feature rich
+    - Class components can maintain their own private data => A.K.A. 'state'
+    - Class components can contain complex UI logic
+    - Class components provide lifecycle hooks (important!)
+    - Class components are also called: "Stateful components", "Smart components", "Container components"
+
+*/
+
 import React, { Component } from "react";
 
-class Welcome extends Component {
+class ClassComponent extends Component {
     render() {
         return <h1>Class Component</h1>
     }
 
 }
 
-export default Welcome;
+export default ClassComponent;
--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,9 +1,21 @@
+/*
+    FUNCTIONAL COMPONENT:
+
+    - Functional components are simple functions, receiving props and returning a declaration
+    - Use these as much as possible
+    - You will not need "this" keyword
+    - You will be forced to think of a solution without using 'state' => Maintanance and debugging will be easier
+    - Functional components usually don't have logic and are mainly responsible for UI
+    - Functional components are also called: "Stateless components", "Dumb components", "Presentational components"
+
+*/
+
 import React from "react";
 
-// function Greet() {
-//     return <h1>Hello Fleur!</h1>
-// }
+function FunctionalComponent1() {
+    return <h1>Functional Component</h1>
+}
 
-const Greet = () => <h1>Hello Fleur!</h1>
+const FunctionalComponent2 = () => <h1>Functional Component</h1>
 
-export default Greet;
+export default FunctionalComponent2;
